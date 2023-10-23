08057d8715094b935ebaf4c32fa5b7d6c2c23f43
Triplesong90 <expert190@outlook.kr>
2023-10-23 17:19:40 +0900

    add save method

--- src/train.py
+++ src/train.py
@@ -4,12 +4,24 @@ from tqdm import tqdm
 from torch.utils.data import WeightedRandomSampler
 from dataset import get_dataloader
 from transforms import train_transforms, val_transforms
+import os
 from efficientnet_pytorch import EfficientNet
+
+
+def save_model(model, image_size, num_classes, model_type, class_names, path):
+    torch.save({
+        'model': model.state_dict(),
+        'image_size': (image_size, image_size),
+        'num_classes': num_classes,
+        'transforms': val_transforms(image_size),
+        'model_type' : model_type,
+        'class_names' : class_names
+    }, path)
 
 image_size = 512
 num_classes = 5
-batch_size = 16
-train_dir = '/dataset'
+batch_size = 20
+train_dir = '/mnt/volume1/strap-classification'
 lr = 0.001
 gamma = 0.9
 epochs = 300
@@ -17,9 +29,12 @@ device = 'cuda'
 model_type = 'efficientnet-b0'
 
 model = EfficientNet.from_pretrained(model_type, num_classes=num_classes)
+trainId = os.environ.get('TRAINID', None)
+model_savr_dir = os.path.join('/mnt/volume2', trainId)
+if trainId is not None:
+    os.makedirs(model_savr_dir)
 
-# checkpoint = torch.load('./model_70.pth')
-# model.load_state_dict(checkpoint['model'])
+print(trainId, model_savr_dir,  f"{model_savr_dir}/model_val_best.pth")
 
 model.to(device)
 
@@ -59,6 +74,8 @@ sampler = WeightedRandomSampler(torch.DoubleTensor(weights), total_cnt)
 train_loader = get_dataloader(data_list, train_transforms(image_size), batch_size=batch_size,shuffle=None, sampler=sampler)
 val_loader = get_dataloader(data_list, val_transforms(image_size), batch_size=batch_size,shuffle=None, sampler=sampler)
 
+min_train_loss = 100
+min_val_loss = 100
 for epoch in range(epochs):
     epoch_loss = 0
     epoch_accuracy = 0
@@ -91,25 +108,27 @@ for epoch in range(epochs):
             acc = (val_output.argmax(dim=1) == label).float().mean()
             epoch_val_accuracy += acc / len(val_loader)
             epoch_val_loss += val_loss / len(val_loader)
+    if epoch_val_loss < min_val_loss:
+        min_val_loss = epoch_val_loss
+        save_model(model, image_size, num_classes, model_type, class_names, f"model_val_best.pth")
+        if trainId is not None:
+            save_path = os.path.join(model_savr_dir, f"model_val_best.pth")
+            save_model(model, image_size, num_classes, model_type, class_names, save_path)
+
+    if epoch_val_loss < min_train_loss:
+        min_train_loss = epoch_val_loss
+        save_model(model, image_size, num_classes, model_type, class_names, f"model_train_best.pth")
+        if trainId is not None:
+            save_path = os.path.join(model_savr_dir, f"model_train_best.pth")
+            save_model(model, image_size, num_classes, model_type, class_names, save_path)
+
     # save model by every 10
     if (epoch + 1) % 10 == 0:
-        torch.save({
-            'model': model.state_dict(),
-            'image_size': (image_size, image_size),
-            'num_classes': num_classes,
-            'transforms': val_transforms(image_size),
-            'model_type' : model_type,
-            'class_names' : ['OK', 'Align', 'MisAttached', 'NotAttached', 'Disheveled']
-        }, f"model_{epoch + 1}.pth")
+        save_model(model, image_size, num_classes, model_type, class_names, f"model_{epoch + 1}.pth")
 
-    print(
-        f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
-    )
-torch.save({
-            'model': model.state_dict(),
-            'image_size': (image_size, image_size),
-            'num_classes': num_classes,
-            'transforms': val_transforms(image_size),
-            'model_type' : model_type,
-            'class_names' : ['OK', 'Align', 'MisAttached', 'NotAttached', 'Disheveled']
-        }, f"model.pth")
+    print( f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n")
+
+    save_model(model, image_size, num_classes, model_type, class_names, f"model.pth")
+    if trainId is not None:
+        save_path = os.path.join(model_savr_dir, f"model.pth")
+        save_model(model, image_size, num_classes, model_type, class_names, save_path)
